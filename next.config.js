854da2501cd41ccc1be5ae9fcb5b0ec23e2ea381
Mud <44410798+MudDev@users.noreply.github.com>
2024-10-17 13:31:05 -0600

    base paths

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
-}
+  basePath: isProd ? '/snapshot-signer' : '',
+  assetPrefix: isProd ? '/snapshot-signer/' : '',
+};
 
 module.exports = nextConfig;
