ed44ffda018951a4eadc6349484eb48c3544d714
Mud <44410798+MudDev@users.noreply.github.com>
2024-10-25 06:10:47 -0600

    Updates

--- _next/static/BcsEbzVENx4MI-5ajmEOX/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/959-da4e20d63863a17b.js","static/chunks/pages/index-85320e05d8a2fac5.js"],"/_error":["static/chunks/pages/_error-a59e2db023c5e431.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
