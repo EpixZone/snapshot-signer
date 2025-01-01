dd808c4f2e71774f5a99fd57f947b9816df4bfe7
MudDev <trademud@gmail.com>
2025-01-01 14:14:01 -0700

    Updates

--- _next/static/vOW8Do6PBFnIrNGWDJxuv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/959-bfb781f8e7dde3f1.js","static/chunks/pages/index-4f6511d97179d751.js"],"/_error":["static/chunks/pages/_error-a59e2db023c5e431.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
